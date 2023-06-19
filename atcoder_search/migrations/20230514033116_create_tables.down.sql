1c5c63062e5f3cda201f002e86db5297f47f5cfa
fjnkt98 <fjnkt98@gmail.com>
2023-06-19 18:19:25 +0900

    fix wrong trigger name

--- atcoder_search/migrations/20230514033116_create_tables.down.sql
+++ atcoder_search/migrations/20230514033116_create_tables.down.sql
@@ -1,13 +1,13 @@
 -- Add down migration script here
 DROP TABLE IF EXISTS problems;
-DROP TRIGGER IF EXISTS refresh_contests_updated_at_step1 ON problems;
-DROP TRIGGER IF EXISTS refresh_contests_updated_at_step2 ON problems;
-DROP TRIGGER IF EXISTS refresh_contests_updated_at_step3 ON problems;
+DROP TRIGGER IF EXISTS refresh_problems_updated_at_step1 ON problems;
+DROP TRIGGER IF EXISTS refresh_problems_updated_at_step2 ON problems;
+DROP TRIGGER IF EXISTS refresh_problems_updated_at_step3 ON problems;
 
 DROP TABLE IF EXISTS contests;
-DROP TRIGGER IF EXISTS refresh_problems_updated_at_step1 ON contests;
-DROP TRIGGER IF EXISTS refresh_problems_updated_at_step2 ON contests;
-DROP TRIGGER IF EXISTS refresh_problems_updated_at_step3 ON contests;
+DROP TRIGGER IF EXISTS refresh_contests_updated_at_step1 ON contests;
+DROP TRIGGER IF EXISTS refresh_contests_updated_at_step2 ON contests;
+DROP TRIGGER IF EXISTS refresh_contests_updated_at_step3 ON contests;
 
 DROP FUNCTION IF EXISTS refresh_updated_at_step1;
 DROP FUNCTION IF EXISTS refresh_updated_at_step2;
