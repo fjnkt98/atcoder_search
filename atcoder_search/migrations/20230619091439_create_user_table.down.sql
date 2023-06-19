7499c92800a51945b7b7ca117d6e515c249f2eed
fjnkt98 <fjnkt98@gmail.com>
2023-06-19 18:55:02 +0900

    add migration

---
+++ atcoder_search/migrations/20230619091439_create_user_table.down.sql
@@ -0,0 +1,4 @@
+DROP TABLE IF EXISTS users;
+DROP TRIGGER IF EXISTS refresh_users_updated_at_step1 ON users;
+DROP TRIGGER IF EXISTS refresh_users_updated_at_step2 ON users;
+DROP TRIGGER IF EXISTS refresh_users_updated_at_step3 ON users;
