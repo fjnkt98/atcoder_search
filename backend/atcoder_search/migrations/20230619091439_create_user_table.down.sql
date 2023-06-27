bd5dfb3c02a24b982b8542e30e298d457cc54f62
fjnkt98 <fjnkt98@gmail.com>
2023-06-27 22:51:19 +0900

    use sqldef instead sqlx-cli

--- backend/atcoder_search/migrations/20230619091439_create_user_table.down.sql
+++
@@ -1,4 +0,0 @@
-DROP TABLE IF EXISTS users;
-DROP TRIGGER IF EXISTS refresh_users_updated_at_step1 ON users;
-DROP TRIGGER IF EXISTS refresh_users_updated_at_step2 ON users;
-DROP TRIGGER IF EXISTS refresh_users_updated_at_step3 ON users;
--- backend/atcoder_search/migrations/20230625040921_create_difficulty_table.down.sql
+++
@@ -1,4 +0,0 @@
-DROP TABLE IF EXISTS difficulties;
-DROP TRIGGER IF EXISTS refresh_difficulties_updated_at_step1 ON difficulties;
-DROP TRIGGER IF EXISTS refresh_difficulties_updated_at_step2 ON difficulties;
-DROP TRIGGER IF EXISTS refresh_difficulties_updated_at_step3 ON difficulties;
